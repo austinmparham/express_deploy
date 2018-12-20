08a6bdcf12597a612cc7e32f56f9655438927778
austinmparham <42558200+austinmparham@users.noreply.github.com>
2018-12-19 20:23:25 -0600

    added portfolio code

--- server.js
+++ server.js
@@ -4,18 +4,17 @@ var app = express();
 app.use(express.static(__dirname + "/static"));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-app.get('/', function(request, response) {
-   response.send("<h1>Hello Express</h1>");
+app.get("/", function (request, response){
+    response.render('about_me');
 })
-app.get("/users", function (request, response){
-    // hard-coded user data
-    var users_array = [
-        {name: "Michael", email: "michael@codingdojo.com"}, 
-        {name: "Jay", email: "jay@codingdojo.com"}, 
-        {name: "Brendan", email: "brendan@codingdojo.com"}, 
-        {name: "Andrew", email: "andrew@codingdojo.com"}
-    ];
-    response.render('users', {users: users_array});
+app.get("/tech_skills", function (request, response){
+    response.render('tech_skills');
+})
+app.get("/projects", function (request, response){
+    response.render('projects');
+})
+app.get("/contact", function (request, response){
+    response.render('contact');
 })
 app.listen(8000, function() {
   console.log("listening on port 8000");
